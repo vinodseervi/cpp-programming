e33dc59a51a705d35bde2e81d914830038aa8aee
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2021-05-13 14:30:28 +0530

    update questions

--- pattern.cpp
+++ pattern.cpp
@@ -1,13 +1,23 @@
-#include<iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 
 
-int main() 
+int main()
 {
-     int a,b,c,d;
-     cin>>a>>b>>c;
-     d=2*((b*c)+(c*a)+(a*b));
-     cout<<d;
-	
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    char arr[9][10]={"one", "two", "three", "four", "five", "six", "seven","eight","nine"};
+    if(n<=9){
+        cout<<arr[n-1];
+    }
+    else if(n>9){
+        cout<<"Greater than 9";
+    }
+
+    // Write Your Code Here
+
+    return 0;
 }
